c3c31e33eca1b19a83fd20081501aebe2861a3d9
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-05-28 08:45:57 -0300

    setup do projeto criado

---
+++ src/main/kotlin/br/com/zup/wagner/Application.kt
@@ -0,0 +1,10 @@
+package br.com.zup.wagner
+
+import io.micronaut.runtime.Micronaut.*
+fun main(args: Array<String>) {
+	build()
+	    .args(*args)
+		.packages("br.com.zup.wagner")
+		.start()
+}
+
